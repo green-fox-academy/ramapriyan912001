8619d419caa8fdec81a62d8caf2fa8d36b1f97d0
Ramapriyan Srivatsan <ramapriyan912001@gmail.com>
2020-12-04 19:02:52 +0800

    End of Day 5

--- week-01/day-5/War/Pirate.java
+++ week-01/day-5/War/Pirate.java
@@ -2,8 +2,6 @@ public class Pirate {
     private int glassOfRumCount;
     private static int pirateCount;
 
-    public Pirates
-
     public void drinkSomeRum() {
         System.out.println("Aye, that's some good rum matey.");
         glassOfRumCount++;
